f9dc49d9d9465c2aae4b6aab7eead334959fb1a1
Vincent <v23zhou@uwaterloo.ca>
2026-03-13 18:00:58 -0400

    Implement CRUD functionality

--- 02_express/server.js
+++ 02_express/server.js
@@ -3,31 +3,82 @@ import express from "express";
 const app=express();
 const port=3000;
 
+const router=express.Router();
+
+app.use(express.json());
+
+let cars=[
+    {id:0, make:"Toyota",model:"Camry", year:2022, price:20000},
+    {id:1, make:"Tesla",model:"X", year:2023, price:25000},
+    {id:2, make:"Ford",model:"F150", year:2025, price:40000}
+];
+
+
 app.get("/", (req,res)=>{
     res.send("Hello from the Cars API");
 });
 
-app.get("/api/v1/cars",(req,res)=>{
-    res.send("All Cars");
+router.get("/",(req,res)=>{
+    res.json(cars)
 });
 
-app.post("/api/v1/cars",(req,res)=>{
+router.get("/:id",(req,res)=>{
+    const id=req.params.id
+    const car=cars.find((car)=>car.id==id);
+
+    if (!car) return res.status(404).send("Car not found")
+    res.json(car)
+});
+
+
+router.post("/",(req,res)=>{
     //extract fields from req.body and validate and insert into database
-    res.send("New Car");
+    const info=req.body
+    if (!(info.make) || !(info.model) || !(info.year) || !(info.price)){
+        return res.status(400).send("No data");
+    }
+    const newCar={
+        id:cars.length,
+        make:info.make,
+        model:info.model,
+        year:info.year,
+        price:info.price
+    }
+    cars.push(newCar)
+    return res.json(cars)
 });
 
-//api/v1/cars/5123, dynamic route
-app.put("/api/v1/cars/:id",(req,res)=>{
-    res.send("Update a car");
+router.put("/:id",(req,res)=>{
+    const info=req.body
+    const id=req.params.id
+    const a=cars.find((car)=> car.id==id)
+    if (!a)
+    {
+        return res.status(404).send("bad id")
+    }
+    const make=info.make
+    const model=info.model
+    const price=info.price
+    const year=info.year
+    a.make= make ? make:a.make
+    a.model= model? model:a.model
+    a.year= year ? year:a.year
+    a.price= price? price:a.price
+    res.json(cars)
 });
 
-app.delete("/api/v1/cars/:id",(req,res)=>{
-    res.send("Delete a car");
+router.delete("/:id",(req,res)=>{
+    const id=req.params.id
+    const index=cars.findIndex((car)=>id==car.id)
+    if (index===-1){
+        return res.status(404).send("Invalid Id")
+    }
+    cars.splice(index,1)
+    return res.json(cars)
 });
 
-app.get("/api/v1/cars/:id",(req,res)=>{
-    res.send("Get a car");
-});
+
+app.use("/api/v1/cars",router)
 
 app.listen(port, ()=>{
     console.log(`server is running on http://localhost:${port}`)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # express-api-playground
 Practicing backend JavaScript development.
+
+Technologies:
+Node.js
+Express
+Rest-Client
